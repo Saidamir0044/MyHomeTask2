71352cd733da1a5b76f417c0bd127aa5a388cdd9
Saidamir0044 <saidamirrajabov2001@gmail.com>
2025-06-13 17:44:11 +0500

    Create geometry.py

---
+++ lesson-11/homework/geometry.py
@@ -0,0 +1,9 @@
+import math_operations as mo
+import string_utils as su
+
+# Test math operations
+print(mo.add(3, 5))           
+print(mo.divide(10, 2))       
+# Test string utilities
+print(su.reverse_string("hello"))    
+print(su.count_vowels("My name is Saidamir"))  
